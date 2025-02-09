ba6ee0e498136b639c43007db89975e64f87c594
srinivas kadalal <kadalalsrinivas2000@gmail.com>
2025-02-08 23:12:00 -0500

    Version check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-6xl font-bold">FlashCode</h1>
+      <h1 className="text-6xl font-bold">FLASHCODE</h1>
     </div>
   );
 }
